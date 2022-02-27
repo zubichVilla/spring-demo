0f81ab8401569d5e29ef5ade9de51e54c3c5d766
zube <zubcic.toni@hotmail.com>
2022-02-27 19:55:51 +0100

    Initial commit

---
+++ src/main/java/com/zube/ApplicationLauncher.java
@@ -0,0 +1,23 @@
+package com.zube;
+
+import org.apache.catalina.Context;
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.Wrapper;
+import org.apache.catalina.startup.Tomcat;
+
+public class ApplicationLauncher {
+
+	public static void main (String[] args) throws LifecycleException {
+
+		Tomcat tomcat = new Tomcat();
+		tomcat.setPort(8080);
+		tomcat.getConnector();
+
+		Context ctx = tomcat.addContext("", null);
+		Wrapper servlet = Tomcat.addServlet(ctx, "myFirstServlet", new MyFancyPdfInvoicesServlet());
+        servlet.setLoadOnStartup(1);
+        servlet.addMapping("/*");//bilo koja putanja
+
+        tomcat.start();
+	}
+}
---
+++ src/main/java/com/zube/MyFancyPdfInvoicesServlet.java
@@ -0,0 +1,33 @@
+package com.zube;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class MyFancyPdfInvoicesServlet extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+		//	getRequestURI()
+		//          Returns the part of this request's URL from the protocol name
+		//          up to the query string in the first line of the HTTP request.
+		if(request.getRequestURI().equalsIgnoreCase("/")){
+			response.setContentType("text/html; charset=UTF-8");
+			response.getWriter().print(
+					"<html>\n" +
+							"<body>\n" +
+							"<h1>Hello World</h1>\n" +
+							"<p>This is my very first, embedded Tomcat, HTML Page!</p>\n" +
+							"</body>\n" +
+							"</html>"
+			);
+		}else if(request.getRequestURI().equalsIgnoreCase("/invoices")){
+			response.setContentType("application/json; charset=UTF-8");
+			response.getWriter().print("[]"); // vracamo prazni kao test
+		}
+
+	}
+
+}
