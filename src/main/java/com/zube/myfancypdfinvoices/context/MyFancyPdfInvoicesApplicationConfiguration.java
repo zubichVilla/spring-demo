441cfa296b04b840874eb3d351c1179f7296a4be
zube <zubcic.toni@hotmail.com>
2022-03-11 19:49:54 +0100

    Scope change UserService
    Added scope - prototype to UserService

--- src/main/java/com/zube/myfancypdfinvoices/context/MyFancyPdfInvoicesApplicationConfiguration.java
+++ src/main/java/com/zube/myfancypdfinvoices/context/MyFancyPdfInvoicesApplicationConfiguration.java
@@ -3,12 +3,15 @@ package com.zube.myfancypdfinvoices.context;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.zube.myfancypdfinvoices.service.InvoiceService;
 import com.zube.myfancypdfinvoices.service.UserService;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class MyFancyPdfInvoicesApplicationConfiguration {
 
+    @Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE)
     @Bean
     public UserService userService(){
         return new UserService();
