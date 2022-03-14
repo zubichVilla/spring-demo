5341335ed12fd5a75cad9c5fe855a08c797da4e7
zube <zubcic.toni@hotmail.com>
2022-03-14 15:27:17 +0100

    Remove Beans
    Beans not needed beacause SPRING creates them automatically

--- src/main/java/com/zube/myfancypdfinvoices/context/MyFancyPdfInvoicesApplicationConfiguration.java
+++ src/main/java/com/zube/myfancypdfinvoices/context/MyFancyPdfInvoicesApplicationConfiguration.java
@@ -2,27 +2,13 @@ package com.zube.myfancypdfinvoices.context;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.zube.myfancypdfinvoices.ApplicationLauncher;
-import com.zube.myfancypdfinvoices.service.InvoiceService;
-import com.zube.myfancypdfinvoices.service.UserService;
-import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Scope;
 
 @Configuration
 @ComponentScan(basePackageClasses = ApplicationLauncher.class)
 public class MyFancyPdfInvoicesApplicationConfiguration {
-
-    @Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE)
-
-    public UserService userService(){
-        return new UserService();
-    }
-
-    public InvoiceService invoiceService(UserService userService) {
-        return new InvoiceService(userService);
-    }
 
     @Bean
     public ObjectMapper objectMapper() {
