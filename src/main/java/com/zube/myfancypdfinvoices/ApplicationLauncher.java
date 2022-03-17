1870ba26c615b3eb9f1ef4c9c37499be2836cdad
zube <zubcic.toni@hotmail.com>
2022-03-17 19:46:24 +0100

    Adding a Controller and DispatcherServlet
    Added Controller and created and registered DispatcherServlet with Tomcat.

--- src/main/java/com/zube/myfancypdfinvoices/ApplicationLauncher.java
+++ src/main/java/com/zube/myfancypdfinvoices/ApplicationLauncher.java
@@ -1,10 +1,17 @@
 package com.zube.myfancypdfinvoices;
 
+import com.zube.myfancypdfinvoices.context.MyFancyPdfInvoicesApplicationConfiguration;
 import com.zube.myfancypdfinvoices.web.MyFancyPdfInvoicesServlet;
 import org.apache.catalina.Context;
 import org.apache.catalina.LifecycleException;
 import org.apache.catalina.Wrapper;
 import org.apache.catalina.startup.Tomcat;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.ServletContext;
 
 public class ApplicationLauncher {
 
@@ -14,11 +21,27 @@ public class ApplicationLauncher {
         tomcat.setPort(8080);
         tomcat.getConnector();
 
-        Context ctx = tomcat.addContext("", null);
-        Wrapper servlet = Tomcat.addServlet(ctx, "myFirstServlet", new MyFancyPdfInvoicesServlet());
+        Context tomcatCtx = tomcat.addContext("", null);
+
+        WebApplicationContext applicationContext = createApplicationContext(tomcatCtx.getServletContext());
+
+        DispatcherServlet dispatcherServlet = new DispatcherServlet(applicationContext);
+
+        Wrapper servlet = Tomcat.addServlet(tomcatCtx, "dispatcherServlet", dispatcherServlet);
         servlet.setLoadOnStartup(1);
         servlet.addMapping("/*");//bilo koja putanja
 
         tomcat.start();
+    }
+
+    public static WebApplicationContext createApplicationContext(ServletContext servletContext) {
+
+        AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
+        ctx.register(MyFancyPdfInvoicesApplicationConfiguration.class);
+        ctx.setServletContext(servletContext);
+        ctx.refresh();
+        ctx.registerShutdownHook();
+
+        return ctx;
     }
 }
