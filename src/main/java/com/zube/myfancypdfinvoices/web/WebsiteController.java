7f642868362dc77a142ec811bf9e05bfb39c83b3
zube <zubcic.toni@hotmail.com>
2022-03-24 19:19:42 +0100

    Added Thymeleaf template and provided variables via Model

--- src/main/java/com/zube/myfancypdfinvoices/web/WebsiteController.java
+++ src/main/java/com/zube/myfancypdfinvoices/web/WebsiteController.java
@@ -1,13 +1,22 @@
 package com.zube.myfancypdfinvoices.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Date;
 
 @Controller
 public class WebsiteController {
 
     @GetMapping("/")
-    public String homepage(){
+    public String homepage(Model model,
+                           @RequestParam(required = false, defaultValue = "stranger") String username)
+    {
+        model.addAttribute("username", username);
+        model.addAttribute("currentDate", new Date());
+
         return "/index.html";
     }
 
